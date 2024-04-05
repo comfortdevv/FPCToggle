8c6a2e1f89331a8f1551b7303b0dce3b24b94c09
comfortdevv <166080401+comfortdevv@users.noreply.github.com>
2024-04-04 19:01:34 -0500

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Main
+{
+    [BepInPlugin("com.comfortdev.fpctoggle", "fpctoggle", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        private bool showGui = true;
+        private GameObject? thirdPersonCamera;
+
+        void Start()
+        {
+            thirdPersonCamera = GameObject.Find("Player Objects/Third Person Camera");
+        }
+
+
+        void OnGUI()
+        {
+            if (showGui)
+            {
+                if (GUI.Button(new Rect(20, 40, 130, 30), "Toggle FPC")) 
+                {
+                    if (thirdPersonCamera != null)
+                    {
+                        thirdPersonCamera.SetActive(!thirdPersonCamera.activeSelf);
+                    }
+                }
+            }
+        }
+    }
+}
